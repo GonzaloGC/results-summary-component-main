c9ed3bb756a2bdb1c8ff20c2b37baa4d5baf553b
Gonzalo <elias.gonzaloguerrero@gmail.com>
2024-06-20 22:21:21 -0400

    added map method for component render

--- src/components/SectionInfo/SectionInfo.jsx
+++ src/components/SectionInfo/SectionInfo.jsx
@@ -4,16 +4,65 @@ import { TitleSectionInfo } from "../TitleSectionInfo/TitleSectionInfo";
 import { Button } from "../Button/Button";
 
 export const SectionInfo = () => {
+  const data = [
+    {
+      nameInfo: "Reaction",
+      primaryNumber: "80",
+      secondNumber: "100",
+      bg: "red",
+      color: "red",
+    },
+    {
+      nameInfo: "Memory",
+      primaryNumber: "92",
+      secondNumber: "100",
+      bg: "yellow",
+      color: "yellow",
+    },
+    {
+      nameInfo: "Verbal",
+      primaryNumber: "61",
+      secondNumber: "100",
+      bg: "green",
+      color: "green",
+    },
+    {
+      nameInfo: "Visual",
+      primaryNumber: "72",
+      secondNumber: "100",
+      bg: "blue",
+      color: "blue",
+    },
+  ];
+
   return (
     <section className="infoSummary">
       <TitleSectionInfo title="Summary" />
       <section className="groupInfoData">
-        <InfoData bg='red' color="red" nameInfo="Reaction" primaryNumber="80" secondNumber="100" />
-        <InfoData bg='yellow' color="yellow" nameInfo="Memory" primaryNumber="92" secondNumber="100" />
-        <InfoData bg='green' color="green" nameInfo="Verbal" primaryNumber="61" secondNumber="100" />
-        <InfoData bg='blue' color="blue" nameInfo="Visual" primaryNumber="72" secondNumber="100" />
+        {data.map((data) => (
+          <div key={data.nameInfo}>
+            <InfoData
+              bg={data.bg}
+              color={data.color}
+              nameInfo={data.nameInfo}
+              primaryNumber={data.primaryNumber}
+              secondNumber={data.secondNumber}
+            />
+          </div>
+        ))}
       </section>
       <Button msg="Continue" />
     </section>
   );
 };
+
+{
+  /* 
+<section className="groupInfoData">
+  <InfoData bg='red' color="red" nameInfo="Reaction" primaryNumber="80" secondNumber="100" />
+  <InfoData bg='yellow' color="yellow" nameInfo="Memory" primaryNumber="92" secondNumber="100" />
+  <InfoData bg='green' color="green" nameInfo="Verbal" primaryNumber="61" secondNumber="100" />
+  <InfoData bg='blue' color="blue" nameInfo="Visual" primaryNumber="72" secondNumber="100" /> 
+</section>
+*/
+}
